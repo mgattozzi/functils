9288d2a36d09b745329327e8d08874b277320d7d
Michael Gattozzi <mgattozzi@gmail.com>
2016-09-18 13:33:33 -0400

    Relicense dually under Apache-2.0/MIT
    To quote cmr on this and having opened up issues on many crates about
    this:
    
    "The MIT license requires reproducing countless copies of the same copyright
    header with different names in the copyright field, for every MIT library in
    use. The Apache license does not have this drawback. However, this is not the
    primary motivation for me creating these issues. The Apache license also has
    protections from patent trolls and an explicit contribution licensing clause.
    However, the Apache license is incompatible with GPLv2. This is why Rust is
    dual-licensed as MIT/Apache (the "primary" license being Apache, MIT only for
    GPLv2 compat), and doing so would be wise for this project."

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+// Copyright 2016 functils Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
 //! Rust has really powerful generics that can be used to make it more functional in terms of it's
 //! composition. Included in this crate are helper functions often times used in languages like
 //! Haskell.
+
+
 #![allow(dead_code)]
 extern crate kinder;
 pub mod list;
